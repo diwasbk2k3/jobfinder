cec61ed0f2f8e6f0f237969cb463c87d97b2077e
KIRAN10077 <kasalawotkiran@gmail.com>
2025-01-29 21:41:50 +0545

    topjobs section and footer added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import HeroSection from './HeroSection'
+import TopJobs from './TopJobs'
+import Footer from './Footer'
 
 function Home() {
   return (
     <div>
         <Header/>
         <HeroSection/>
+        <TopJobs/>
+        <Footer/>
     </div>
   )
 }
