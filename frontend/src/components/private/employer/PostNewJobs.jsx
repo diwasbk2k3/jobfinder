0bd3c753470fe5d0d435cbaf797f61e967733c4f
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-02-28 13:25:48 +0545

    post jobs updated

--- frontend/src/components/private/employer/PostNewJobs.jsx
+++ frontend/src/components/private/employer/PostNewJobs.jsx
@@ -10,7 +10,6 @@ function PostNewJob() {
     description: "",
     skills: "",
     deadline: "",
-    logo: null,
   });
 
   const handleChange = (e) => {
@@ -38,10 +37,10 @@ function PostNewJob() {
             <input type="text" name="jobTitle" value={formData.jobTitle} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
             <i className="fas fa-briefcase text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
           </div>
-          {/* Company Name */}
+          {/* Owner Name */}
           <div className="relative">
-            <label className="block font-semibold text-gray-700">Company Name</label>
-            <input type="text" name="companyName" value={formData.companyName} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
+            <label className="block font-semibold text-gray-700">Owner Name</label>
+            <input type="text" name="ownerName" value={formData.companyName} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
             <i className="fas fa-building text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
           </div>
           {/* Job Type */}
@@ -56,44 +55,49 @@ function PostNewJob() {
             </select>
             <i className="fas fa-user-tie text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
           </div>
+
           {/* Location */}
           <div className="relative">
             <label className="block font-semibold text-gray-700">Location</label>
             <input type="text" name="location" value={formData.location} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
             <i className="fas fa-map-marker-alt text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
           </div>
-          {/* Salary Range */}
+          {/* Salary */}
           <div className="relative">
-            <label className="block font-semibold text-gray-700">Salary Range</label>
-            <select name="salary" value={formData.salary} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none">
-              <option value=""></option>
-              <option value="Below 20000">Below 20,000</option>
-              <option value="20000-39999">20,000 - 39,999</option>
-              <option value="40000-59999">40,000 - 59,999</option>
-              <option value="60000-79999">60,000 - 79,999</option>
-              <option value="80000-100000">80,000 - 100,000</option>
-              <option value="Negotiable">Negotiable</option>
-            </select>
+            <label className="block font-semibold text-gray-700">Salary</label>
+            <input type="date" name="salary" onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
             <i className="fas fa-dollar-sign text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
           </div>
-          {/* Application Deadline */}
-          <div className="relative">
-            <label className="block font-semibold text-gray-700">Application Deadline</label>
-            <input type="date" name="deadline" value={formData.deadline} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
-            <i className="fas fa-calendar-alt text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
-          </div>
-          {/* Job Description */}
-          <div className="md:col-span-2 relative">
-            <label className="block font-semibold text-gray-700">Job Description</label>
-            <textarea name="description" value={formData.description} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg h-40 bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none"></textarea>
-            <i className="fas fa-file-alt text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
-          </div>
+
           {/* Required Skills */}
           <div className="md:col-span-2 relative">
             <label className="block font-semibold text-gray-700">Required Skills (comma-separated)</label>
             <input type="text" name="skills" value={formData.skills} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
             <i className="fas fa-user-alt text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
           </div>
+
+          {/* Job Description */}
+          <div className="md:col-span-2 relative">
+            <label className="block font-semibold text-gray-700">Job Description</label>
+            <textarea name="description" value={formData.description} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg h-40 bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none"></textarea>
+            <i className="fas fa-file-alt text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
+          </div>
+
+          {/* Application Deadline */}
+          <div className="relative">
+            <label className="block font-semibold text-gray-700">Application Deadline</label>
+            <input type="date" name="deadline" value={formData.deadline} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none" />
+            <i className="fas fa-calendar-alt text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/90"></i>
+          </div>
+          {/* Status */}
+          <div className="md:col-span-2 relative">
+            <label className="block font-semibold text-gray-700">Status</label>
+            <select name="status" value={formData.status} onChange={handleChange} required className="w-full p-3 pl-10 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-purple-400 focus:outline-none">
+              <option value="Open">Open</option>
+              <option value="Closed">Closed</option>
+            </select>
+          </div>
+
           {/* Submit Button */}
           <div className="md:col-span-2 flex justify-center">
             <button type="submit" className="w-full bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white py-3 rounded-lg hover:opacity-70 transition font-semibold shadow-md cursor-pointer">Post Job</button>
