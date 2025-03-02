8469319b3844bd0bc591326b000b65ffcce947f1
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-03-02 12:40:14 +0545

    generate applicant ID

--- backend/routes/applicantRoutes.js
+++ backend/routes/applicantRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Applicant = require('../models/applicantModel');
 
+// Generate Applicant ID
+function generateApplicantId() {
+    const applicantId = Math.floor(1000 + Math.random() * 9000);
+    return `A${applicantId}`;
+}
 
 // Get all applicants
 router.get('/', async (req, res) => {
