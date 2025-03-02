a5fb6c4bc60ca8012be0c705653493778bc0d4b9
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-03-03 01:37:20 +0545

    AdminLayout

--- frontend/src/components/private/admin/AdminLayout.jsx
+++ frontend/src/components/private/admin/AdminLayout.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Navigate  } from 'react-router-dom'
 import AdminNavbar from './AdminNavbar'
 
 function AdminLayout() {
+  const isAdminLoggedIn = localStorage.getItem("admin_email") !== null;
+    if (!isAdminLoggedIn) {
+      return <Navigate to="/admin/login" replace />;
+    }
   return (
     <div>
       <AdminNavbar/>
